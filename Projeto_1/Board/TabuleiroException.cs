1752d73b0ae6fe8f3580766085d80232e674fb72
Fena <fe.oorschot@gmail.com>
2022-11-19 02:55:51 -0300

    Criada validação de posição da peça

---
+++ Projeto_1/Board/TabuleiroException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_1.Board
+{
+    internal class TabuleiroException : Exception
+    {
+        public TabuleiroException(string message) : base(message)
+        {
+
+        }
+    }
+}
