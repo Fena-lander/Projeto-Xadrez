68b566d4cdf3dc44ac46b072f2819b2ed104f95d
Fena <fe.oorschot@gmail.com>
2022-11-19 11:57:05 -0300

    Criando a posição no xadrez

--- Projeto_1/Program.cs
+++ Projeto_1/Program.cs
@@ -2,17 +2,7 @@
 using Projeto_1;
 using Projeto_1.Chess;
 
-try
-{
-    Tabuleiro tab = new Tabuleiro(8, 8);
+PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-    Tela.ImprimirTabuleiro(tab);
-}
-catch(TabuleiroException e)
-{
-    Console.WriteLine(e.Message);
-}
+Console.WriteLine(pos);
+Console.WriteLine(pos.ToPosicao());
