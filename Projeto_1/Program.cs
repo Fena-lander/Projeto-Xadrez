6e69d4d7ad142b406f979cdd35aea4ea6253fb50
Fena <fe.oorschot@gmail.com>
2022-11-21 01:43:17 -0300

    Criado separção de peças e implementando no tabuleiro

--- Projeto_1/Program.cs
+++ Projeto_1/Program.cs
@@ -2,7 +2,20 @@
 using Projeto_1;
 using Projeto_1.Chess;
 
-PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+try
+{
+    Tabuleiro tab = new Tabuleiro(8, 8);
 
-Console.WriteLine(pos);
-Console.WriteLine(pos.ToPosicao());
+    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+    tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 4));
+
+
+    Tela.ImprimirTabuleiro(tab);
+}
+catch(TabuleiroException e)
+{
+    Console.WriteLine(e.Message);
+}
--- Projeto_1/Tela.cs
+++ Projeto_1/Tela.cs
@@ -13,6 +13,7 @@ namespace Projeto_1
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write($"{8 - i} ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -21,12 +22,28 @@ namespace Projeto_1
                     }
                     else
                     {
-                        Console.Write($"{tab.peca(i, j)} ");
+                        ImprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h ");
         }
 
+        public static void ImprimirPeca(Peca peca)
+        {
+            if(peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+        }
     }
 }
