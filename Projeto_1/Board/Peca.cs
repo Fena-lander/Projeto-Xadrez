9c4d6d0bbd307f949b987e660b2d30c2ddcc715c
Fena <fe.oorschot@gmail.com>
2022-11-26 14:40:20 -0300

    Jogo de xadrez básico completo

--- Projeto_1/Board/Peca.cs
+++ Projeto_1/Board/Peca.cs
@@ -46,7 +46,7 @@ namespace Projeto_1.Board
             return false;
         }
 
-        public bool PodeMoverPara(Posicao pos)
+        public bool MovimentoPossivel(Posicao pos)
         {
             return MovimentosPossiveis()[pos.Linha, pos.Coluna];
         }
---
+++ Projeto_1/Chess/Peao.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto_1.Board;
+
+namespace Projeto_1.Chess
+{
+    internal class Peao : Peca
+    {
+        private PartidaDeXadrez partida;
+        public Peao(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(cor, tab)
+        {
+            this.partida = partida;
+        }
+        public override string ToString()
+        {
+            return "P";
+        }
+        private bool ExisteInimigo(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p != null && p.Cor != Cor;
+        }
+
+        private bool Livre(Posicao pos)
+        {
+            return Tab.peca(pos) == null;
+        }
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            if (Cor == Cor.Branca)
+            {
+                pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+                if (Tab.PosicaoValida(pos) && Livre(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna);
+                Posicao p2 = new Posicao(Posicao.Linha - 1, Posicao.Coluna);
+                if(Tab.PosicaoValida(p2) && Livre(p2) && Tab.PosicaoValida(pos) && Livre(pos) && Movimentos == 0)
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+                if(Tab.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+                if(Tab.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+            }
+            else
+            {
+                pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+                if(Tab.PosicaoValida(pos) && Livre(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna);
+                Posicao p2 = new Posicao(Posicao.Linha + 1, Posicao.Coluna);
+                if(Tab.PosicaoValida(p2) && Livre(p2) && Tab.PosicaoValida(pos) && Livre(pos) && Movimentos == 0)
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+                if(Tab.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+                if(Tab.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+            }
+            return mat;
+        }
+    }
+}
